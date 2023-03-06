fedc29709cdc0dd112784c596570192d1c844f6b
Nur Damia Sharizal <126445971+miashazal@users.noreply.github.com>
2023-03-06 22:34:14 +0800

    Update TestFileReader.java

--- TestFileReader.java
+++ TestFileReader.java
@@ -1,4 +1,4 @@
-package com.example.mathcompetency
+package com.example.mathcompetency;
 
 import java.io.File;  // Import the File class
 import java.io.FileNotFoundException;  // Import this class to handle errors
